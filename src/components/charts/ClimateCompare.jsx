06fdd081fd99596fe25b8fc9c09f9a3907fb0c0c
Martha Hinrichs <marthaghinrichs@gmail.com>
2025-05-10 18:33:52 -0400

    use svg for temperature compare chart

--- src/components/charts/ClimateCompare.jsx
+++ src/components/charts/ClimateCompare.jsx
@@ -121,107 +121,62 @@ export default function ClimateCompare({ data1, data2, city1Color = 'rgba(194, 1
                         </Group>
                     ))}
                 </Stack>
-                <div style={{ position: 'relative', flex: 1, height: '100%' }}>
-                    {/* Decorative layer */}
-                    <div style={{
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        width: '100%',
-                        height: '100%',
-                        zIndex: 2,
-                        display: 'flex', 
-                        alignItems: 'stretch', 
-                        justifyContent: 'space-evenly',
-                        pointerEvents: 'none'
-                    }}>
-                        {months1.map(m => (
-                            <div key={`decorative-${m}`} style={{ position: 'relative', width: '100%', height: '100%', margin: '0px 1px', display: 'inline-block' }}>
-                                <div style={{
-                                    position: 'absolute',
-                                    top: `${50 - data1.weather[m].temperature.high}%`,
-                                    height: `${(data1.weather[m].temperature.high - data1.weather[m].temperature.low)}%`,
-                                    width: '100%',
-                                    borderRadius: '20%',
-                                    border: clickedCities.has(data1.city) ? '2px solid #9810fa' : 'none',
-                                    boxShadow: `0 0 5px #9810fa, 0 0 10px #9810fa, inset 0 0 3px #9810fa`
-                                }} />
-                                <div style={{
-                                    position: 'absolute',
-                                    top: `${50 - data2.weather[months2[months1.indexOf(m)]].temperature.high}%`,
-                                    height: `${(data2.weather[months2[months1.indexOf(m)]].temperature.high - data2.weather[months2[months1.indexOf(m)]].temperature.low)}%`,
-                                    width: '100%',
-                                    borderRadius: '20%',
-                                    border: clickedCities.has(data2.city) ? '2px solid #00a63e' : 'none',
-                                    boxShadow: `0 0 5px #00a63e, 0 0 10px #00a63e, inset 0 0 3px #00a63e`
-                                }} />
-                            </div>
-                        ))}
-                    </div>
-
+                <div style={{ position: 'relative', flex: 1, height: '100%', paddingLeft: 40, paddingRight: 20 }}>
                     {/* Temperature visualization with gradient */}
-                    <div style={{ 
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        height: '100%', 
-                        width: '100%', 
-                        display: 'flex', 
-                        alignItems: 'stretch', 
-                        justifyContent: 'space-evenly',
-                        background: `linear-gradient(to bottom,
-                            #ed6bff 0%,
-                            #ff6467 8%,
-                            #ff6467 15%,
-                            #fe9a00 25%,
-                            #9ae600 35%,
-                            #05df72 40%,
-                            #05df72 45%,
-                            #00d3f3 70%,
-                            #51a2ff 80%,
-                            #7c86ff 90%,
-                            #8e51ff 100%
-                        )`,
-                        filter: 'brightness(5)',
-                        zIndex: 1
-                    }}>
-                        {months1.map(m => (
-                            <div key={m} style={{ position: 'relative', width: '100%', margin: '0px 1px' }}>
-                                <div style={{
-                                    position: 'absolute',
-                                    top: `${50 - data1.weather[m].temperature.high}%`,
-                                    height: `${(data1.weather[m].temperature.high - data1.weather[m].temperature.low)}%`,
-                                    width: '100%',
-                                    borderRadius: '20%'
-                                }}>
-                                    <div style={{
-                                        width: '100%',
-                                        height: '100%',
-                                        background: clickedCities.has(data1.city) || hoveredCity === data1.city || (!clickedCities.size && hoveredCity === null) ? 'rgba(0, 0, 255, 0.75)' : 'transparent',
-                                        borderRadius: '20%',
-                                        filter: 'brightness(0.0001)',
-                                        mixBlendMode: 'multiply'
-                                    }} />
-                                </div>
-                                <div style={{
-                                    position: 'absolute',
-                                    top: `${50 - data2.weather[months2[months1.indexOf(m)]].temperature.high}%`,
-                                    height: `${(data2.weather[months2[months1.indexOf(m)]].temperature.high - data2.weather[months2[months1.indexOf(m)]].temperature.low)}%`,
-                                    width: '100%',
-                                    borderRadius: '20%'
-                                }}>
-                                    <div style={{
-                                        width: '100%',
-                                        height: '100%',
-                                        background: clickedCities.has(data2.city) || hoveredCity === data2.city || (!clickedCities.size && hoveredCity === null) ? 'rgba(0, 0, 255, 0.75)' : 'transparent',
-                                        borderRadius: '20%',
-                                        filter: 'brightness(0.0001)',
-                                        mixBlendMode: 'multiply'
-                                    }} />
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                    <svg width="100%" height="50vh" viewBox="0 0 1000 500" preserveAspectRatio="none">
+                    {/* Gradient Definition */}
+                    <defs>
+                        <linearGradient id="temperatureGradient" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="0%" stopColor="#ed6bff" />
+                            <stop offset="8%" stopColor="#ff6467" />
+                            <stop offset="15%" stopColor="#ff6467" />
+                            <stop offset="25%" stopColor="#fe9a00" />
+                            <stop offset="35%" stopColor="#9ae600" />
+                            <stop offset="40%" stopColor="#05df72" />
+                            <stop offset="45%" stopColor="#05df72" />
+                            <stop offset="70%" stopColor="#00d3f3" />
+                            <stop offset="80%" stopColor="#51a2ff" />
+                            <stop offset="90%" stopColor="#7c86ff" />
+                            <stop offset="100%" stopColor="#8e51ff" />
+                        </linearGradient>
+
+                        {/* Mask Definition */}
+                        {/* MASK: white = clear, black = full white veil, gray = partial veil */}
+                        <mask id="bar-mask" mask-type="luminance">
+                        {/* Entire mask is black = full white veil */}
+                        <rect x="0" y="0" width="100%" height="100%" fill="white" fill-opacity="1" />
+                        
+                        {/* Bars act like "light leaks" in the veil */}
+                        {months.map((m, i) => {
+                            const x = (100/12) * i;
+                            const w = `calc((100vw - 100px) / 12)`;
+                            const h1 = (data1.weather[m].temperature.high - data1.weather[m].temperature.low);
+                            const h2 = (data2.weather[m].temperature.high - data2.weather[m].temperature.low);
+                            const y1 = 50 - data1.weather[months2[months1.indexOf(m)]].temperature.high;
+                            const y2 = 50 - data2.weather[months1[months1.indexOf(m)]].temperature.high;
+
+                            return (
+                            <g key={i}>
+                                <rect x={`${x}%`} y={`${y1}%`} width={w} height={`${h1}%`} fill="black" 
+                                    fillOpacity={clickedCities.has(data1.city) || hoveredCity === data1.city ? 0.75 : 0.5} 
+                                />
+                                <rect x={`${x}%`} y={`${y2}%`} width={w} height={`${h2}%`} fill="black" 
+                                    fillOpacity={clickedCities.has(data2.city) || hoveredCity === data2.city ? 0.75 : 0.5}
+                                />
+                            </g>
+                            );
+                        })}
+                        </mask>
+                    </defs>
+
+                    {/* Underlying Gradient */}
+                    <rect x="0" y="0" width="100%" height="100%" fill="url(#temperatureGradient)" />
+
+                    {/* White layer with punched-out bars */}
+                    <rect x="0" y="0" width="100%" height="100%" fill="white" mask="url(#bar-mask)" />
+
+                </svg>
+
                     <div style={{position: 'absolute', right: 0, top: -40, width: '100%', zIndex: 5, display: 'flex', justifyContent: 'space-evenly', textAlign: 'center'}}>
                         <button 
                             onClick={() => handleMonthOffset(setMonths1, 'left')}
